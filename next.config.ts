70553a5301afd6930a608510f0588c7acd0a9dc3
Eugene Cross <eugene.cross@gmail.com>
2026-05-12 19:48:01 +0100

    new lan scripts to fix webcam nav issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // next.config.js
+  allowedDevOrigins: ['192.168.164.103'],
 };
 
 export default nextConfig;
